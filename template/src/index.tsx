81ec8d6219b655d8eecbfb30020e5398a0ddb792
haqadosch <haqadosch@pm.me>
2020-02-24 13:03:53 +0000

    Add bare bone counter as default app

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,45 +1,50 @@
-import React, { ErrorInfo, StrictMode } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { ErrorInfo } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { App } from './App'
+import * as serviceWorker from './serviceWorker'
+
+import { Provider } from "react-redux"
+import { store } from "./Store/store"
 
 class ErrorBoundary extends React.Component<{}, { hasError: boolean }, {}> {
-  constructor(props: any) {
+  constructor (props: any) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { hasError: false }
   }
 
-  static getDerivedStateFromError(error: Error) {
+  static getDerivedStateFromError (error: Error) {
     // Update state so the next render will show the fallback UI.
-    return { hasError: true };
+    return { hasError: true }
   }
 
-  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+  componentDidCatch (error: Error, errorInfo: ErrorInfo) {
     // You can also log the error to an error reporting service
-    console.error({ error, errorInfo });
+    console.error(error, errorInfo)
   }
 
-  render() {
+  render () {
     if (this.state.hasError) {
       // You can render any custom fallback UI
       return <h1>Something went wrong.</h1>;
     }
 
-    return this.props.children;
+    return this.props.children
   }
 }
 
 ReactDOM.render(
-  <StrictMode>
+  <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <Provider store={ store }>
+        <App />
+      </Provider>
     </ErrorBoundary>
-  </StrictMode>,
-  document.getElementById('root'),
-);
+  </React.StrictMode>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register()
