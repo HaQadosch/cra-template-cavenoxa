023df08a097206590eb511e938178c8ad8414e3e
HaQadosch Berraka <haqadosch@pm.me>
2020-03-29 10:19:42 +0100

    react: add error boundaries from react-error-boundaries package

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,4 +1,4 @@
-import React, { ErrorInfo } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { App } from './App'
@@ -7,38 +7,11 @@ import * as serviceWorker from './serviceWorker'
 import { Provider } from "react-redux"
 import { store } from "./store/store"
 
-class ErrorBoundary extends React.Component<{}, { hasError: boolean }, {}> {
-  state = {
-    hasError: false
-  }
-
-  static getDerivedStateFromError (error: Error) {
-    // Update state so the next render will show the fallback UI.
-    return { hasError: true }
-  }
-
-  componentDidCatch (error: Error, errorInfo: ErrorInfo) {
-    // You can also log the error to an error reporting service
-    console.error(error, errorInfo)
-  }
-
-  render () {
-    if (this.state.hasError) {
-      // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
-    }
-
-    return this.props.children
-  }
-}
-
 ReactDOM.render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <Provider store={ store }>
-        <App />
-      </Provider>
-    </ErrorBoundary>
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
